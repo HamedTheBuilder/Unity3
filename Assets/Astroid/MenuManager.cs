66d0321d903f89b4dc926d052e382a3f4b79b973
YazDev0 <yazed.marshad@hotmail.com>
2025-10-16 13:05:04 +0300

    change physics

--- Assets/Astroid/MenuManager.cs
+++ Assets/Astroid/MenuManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
@@ -6,5 +6,19 @@ public class PlayButton : MonoBehaviour
     public void LoadGameScene()
     {
         SceneManager.LoadScene("Astroid");
+    }
+
+
+    public void QuitGame()
+    {
+        // إغلاق اللعبة
+        Application.Quit();
+
+        // في المحرر يتوقف اللعب
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+        Debug.Log("🎮 اللعبة انتهت!");
     }
 }
